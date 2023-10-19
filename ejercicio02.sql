e893c34d1c525913a40b378964d00f5b6553befd
Daniel Nuñez Romero <146624324+Daninuro@users.noreply.github.com>
2023-10-19 15:26:10 +0200

    Push ejercicio2

---
+++ ejercicio02.sql
@@ -0,0 +1,71 @@
+drop database if exists ejercicio2;
+create database if not exists ejercicio2;
+use ejercicio2;
+
+create table if not exists Empresa (
+	CIF varchar(20) PRIMARY KEY,
+    direccion varchar(255),
+    telefono varchar(12),
+    edad int
+);
+
+create table if not exists Alumno (
+	DNI varchar(20) PRIMARY KEY,
+    nombre varchar(255),
+    apellidos varchar(255),
+    direccion varchar(255),
+    telefono varchar(12),
+    edad int
+);
+
+create table if not exists Trabajar(
+	fk_CIF varchar(10),
+    fk_DNI varchar(10),
+    foreign key (fk_CIF) REFERENCES Empresa(CIF),
+    foreign key (fk_DNI) references Alumno(DNI)
+);
+
+create table if not exists Profesor (
+	DNI varchar(20) PRIMARY KEY,
+    nombre varchar(255),
+    apellidos varchar(255),
+    direccion varchar(255),
+    telefono varchar(12)
+);
+
+create table if not exists Curso (
+	codigo varchar(20) PRIMARY KEY,
+    programa varchar(255),
+    duracion time,
+    titulo varchar(255),
+    numero int
+);
+
+create table if not exists Impartir(
+	fk_DNI varchar(10),
+    fk_codigo varchar(10),
+    foreign key (fk_DNI) REFERENCES Profesor(DNI),
+    foreign key (fk_codigo) references Curso(codigo)
+);
+
+create table if not exists Cursar(
+	fk_DNI varchar(10),
+    fk_codigo varchar(10),
+    foreign key (fk_codigo) REFERENCES Curso(codigo),
+    foreign key (fk_DNI) references Alumno(DNI)
+);
+
+create table if not exists DatosCurso (
+	codigo varchar(20) PRIMARY KEY,
+    programa varchar(255),
+    duracion time,
+    titulo varchar(255)
+);
+
+create table if not exists Corresponder(
+	fk_codigo_curso varchar(10),
+    fk_codigo_datos varchar(10),
+    foreign key (fk_codigo_curso) REFERENCES Curso(codigo),
+    foreign key (fk_codigo_datos) references DatosCurso(codigo)
+);
+
