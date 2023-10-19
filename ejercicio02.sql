2c96044eab2b79f3a7959abfc09801b882c1da0f
Daniel Nuñez Romero <146624324+Daninuro@users.noreply.github.com>
2023-10-19 16:12:10 +0200

    Push ejercicio02

--- ejercicio02.sql
+++ ejercicio02.sql
@@ -21,8 +21,13 @@ create table if not exists Alumno (
 create table if not exists Trabajar(
 	fk_CIF varchar(10),
     fk_DNI varchar(10),
-    foreign key (fk_CIF) REFERENCES Empresa(CIF),
+    foreign key (fk_CIF) REFERENCES Empresa(CIF)
+		ON DELETE CASCADE
+    ON UPDATE CASCADE,
     foreign key (fk_DNI) references Alumno(DNI)
+		ON DELETE CASCADE
+    ON UPDATE CASCADE
+    
 );
 
 create table if not exists Profesor (
@@ -44,15 +49,23 @@ create table if not exists Curso (
 create table if not exists Impartir(
 	fk_DNI varchar(10),
     fk_codigo varchar(10),
-    foreign key (fk_DNI) REFERENCES Profesor(DNI),
+    foreign key (fk_DNI) REFERENCES Profesor(DNI)
+    		ON DELETE CASCADE
+    ON UPDATE CASCADE,
     foreign key (fk_codigo) references Curso(codigo)
+    		ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 create table if not exists Cursar(
 	fk_DNI varchar(10),
     fk_codigo varchar(10),
-    foreign key (fk_codigo) REFERENCES Curso(codigo),
+    foreign key (fk_codigo) REFERENCES Curso(codigo)
+    		ON DELETE CASCADE
+    ON UPDATE CASCADE,
     foreign key (fk_DNI) references Alumno(DNI)
+    		ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
 create table if not exists DatosCurso (
@@ -65,7 +78,11 @@ create table if not exists DatosCurso (
 create table if not exists Corresponder(
 	fk_codigo_curso varchar(10),
     fk_codigo_datos varchar(10),
-    foreign key (fk_codigo_curso) REFERENCES Curso(codigo),
+    foreign key (fk_codigo_curso) REFERENCES Curso(codigo)
+    		ON DELETE CASCADE
+    ON UPDATE CASCADE,
     foreign key (fk_codigo_datos) references DatosCurso(codigo)
+    		ON DELETE CASCADE
+    ON UPDATE CASCADE
 );
 
