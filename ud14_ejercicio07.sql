3f6cbce10358dbe79d92fa045ff22889c42f6699
Nayara Gomes <nay.nivea@gmail.com>
2023-10-19 17:47:57 +0200

    Ejercicio 07 UD 14

---
+++ ud14_ejercicio07.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS Ejercicio07;
+CREATE DATABASE IF NOT EXISTS Ejercicio07;
+
+USE Ejercicio07;
+
+CREATE TABLE IF NOT EXISTS Despacho (
+    Numero INT PRIMARY KEY,
+    Capacidad INT
+);
+
+CREATE TABLE IF NOT EXISTS Director (
+    DNI VARCHAR(8) PRIMARY KEY NOT NULL,
+    NomApels NVARCHAR(255),
+    DNI_Jefe VARCHAR(8),
+    Num_Despacho INT,
+    FOREIGN KEY (DNI_Jefe)
+        REFERENCES Director (DNI),
+    FOREIGN KEY (Num_Despacho)
+        REFERENCES Despacho (Numero)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
