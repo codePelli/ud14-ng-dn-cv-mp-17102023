3ce01a905effb3ed8b4cd7a68910839df2d58688
Daniel Nuñez Romero <146624324+Daninuro@users.noreply.github.com>
2023-10-20 15:55:53 +0200

    Push Ejercicio15

--- ejercicio15.sql
+++ ejercicio15.sql
@@ -1,25 +1,60 @@
+drop database if exists ejercicio15;
 create database if not exists ejercicio15;
 use ejercicio15;
 
 create table if not exists Menu (
-    id_menu int primary key auto_increment,
-    nombre varchar(100),
-    unidad_medida varchar(50)
+	id_menu varchar(20) primary key,
+	fecha date,
+    num_personas int
 );
 
-create table if not exists  Plato (
-    id_plato int primary key auto_increment,
-    nombre varchar(255),
-    tipo varchar(255),
-    descripcion text(255)
+create table if not exists Plato (
+	id_plato varchar(20) primary key,
+	nombre varchar(40),
+    tipo varchar(15),
+    descripcion text,
+    temperatura varchar(5)
 );
 
-create table if not exists  Compone (
-    fk_id_menu int,
-    fk_id_plato int,
-	foreign key (fk_id_menu) references Menu(id_menu)    ON DELETE CASCADE
+create table if not exists Compone (
+	fk_id_menu varchar(20),
+	fk_id_plato varchar(20),
+    primary key (fk_id_menu,fk_id_plato),
+    foreign key (fk_id_menu) references Menu(id_menu)
+    ON DELETE CASCADE
     ON UPDATE CASCADE,
-    foreign key (fk_id_plato) references Plato(id_plato)
-        ON DELETE CASCADE
+	foreign key (fk_id_plato) references Plato(id_plato)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+create table if not exists Dia (
+	fecha date primary key,
+    num_personas int
+);
+
+create table if not exists Sirve (
+	fk_id_menu varchar(20),
+	fk_fecha_sirve date,
+    primary key (fk_id_menu,fk_fecha_sirve),
+    foreign key (fk_id_menu) references Menu(id_menu)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+	foreign key (fk_fecha_sirve) references Dia(fecha)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+create table if not exists Consume (
+	fk_fecha_consume date,
+	fk_id_plato varchar(20),
+    cantidad int,
+    plato_exitoso varchar(40),
+    primary key (fk_fecha_consume,fk_id_plato),
+    foreign key (fk_fecha_consume) references Dia(fecha)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+	foreign key (fk_id_plato) references Plato(id_plato)
+    ON DELETE CASCADE
     ON UPDATE CASCADE
 );
