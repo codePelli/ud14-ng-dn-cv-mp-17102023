28d60b38d5e57398d8f33b3f8df4313834763a08
carlavelasco15 <carlavelasco15@gmail.com>
2023-10-20 18:20:55 +0200

    Update ejercicio15.sql

--- ejercicio15.sql
+++ ejercicio15.sql
@@ -2,30 +2,30 @@ drop database if exists ejercicio15;
 create database if not exists ejercicio15;
 use ejercicio15;
 
-create table if not exists Menu (
-	id_menu varchar(20) primary key,
-	fecha date,
-    num_personas int
+CREATE TABLE IF NOT EXISTS Menu (
+    id_menu VARCHAR(20) PRIMARY KEY,
+    fecha DATE,
+    num_personas INT
 );
 
-create table if not exists Plato (
-	id_plato varchar(20) primary key,
-	nombre varchar(40),
-    tipo varchar(15),
-    descripcion text,
-    temperatura varchar(5)
+CREATE TABLE IF NOT EXISTS Plato (
+    id_plato VARCHAR(20) PRIMARY KEY,
+    nombre VARCHAR(40),
+    tipo VARCHAR(15),
+    descripcion TEXT,
+    temperatura VARCHAR(5)
 );
 
-create table if not exists Compone (
-	fk_id_menu varchar(20),
-	fk_id_plato varchar(20),
-    primary key (fk_id_menu,fk_id_plato),
-    foreign key (fk_id_menu) references Menu(id_menu)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-	foreign key (fk_id_plato) references Plato(id_plato)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS Compone (
+    fk_id_menu VARCHAR(20),
+    fk_id_plato VARCHAR(20),
+    PRIMARY KEY (fk_id_menu , fk_id_plato),
+    FOREIGN KEY (fk_id_menu)
+        REFERENCES Menu (id_menu)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_plato)
+        REFERENCES Plato (id_plato)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 create table if not exists Dia (
@@ -34,28 +34,28 @@ create table if not exists Dia (
 	fecha date
 );
 
-create table if not exists Sirve (
-	fk_id_menu varchar(20),
-	fk_fecha_sirve date,
-    primary key (fk_id_menu,fk_fecha_sirve),
-    foreign key (fk_id_menu) references Menu(id_menu)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-	foreign key (fk_fecha_sirve) references Dia(id_dia)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS Sirve (
+    fk_id_menu VARCHAR(20),
+    fk_fecha_sirve DATE,
+    PRIMARY KEY (fk_id_menu , fk_fecha_sirve),
+    FOREIGN KEY (fk_id_menu)
+        REFERENCES Menu (id_menu)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_fecha_sirve)
+        REFERENCES Dia (id_dia)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-create table if not exists Consume (
-	fk_id_dia date,
-	fk_id_plato varchar(20),
-    cantidad int,
-    plato_exitoso varchar(40),
-    primary key (fk__id_dia,fk_id_plato),
-    foreign key (fk_id_dia) references Dia(id_dia)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-	foreign key (fk_id_plato) references Plato(id_plato)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS Consume (
+    fk_id_dia DATE,
+    fk_id_plato VARCHAR(20),
+    cantidad INT,
+    plato_exitoso VARCHAR(40),
+    PRIMARY KEY (fk__id_dia , fk_id_plato),
+    FOREIGN KEY (fk_id_dia)
+        REFERENCES Dia (id_dia)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_plato)
+        REFERENCES Plato (id_plato)
+        ON DELETE CASCADE ON UPDATE CASCADE
 );
