6b75e4f7d6fff767d396a2f3ee3a6214c55eb01d
carlavelasco15 <carlavelasco15@gmail.com>
2023-10-20 17:49:38 +0200

    correccion ej 16

--- ejercicio16.sql
+++ ejercicio16.sql
@@ -13,43 +13,46 @@ CREATE TABLE IF NOT EXISTS Usuario (
 );
 
 CREATE TABLE IF NOT EXISTS TrendingTopic (
-    Hashtag VARCHAR(100) PRIMARY KEY NOT NULL,
+    id_trending_topic VARCHAR(100) PRIMARY KEY NOT NULL,
     Caracteres VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS Tweet (
     Id_Tweet INT PRIMARY KEY AUTO_INCREMENT,
-    Id_Usuario INT,
-    Hashtag VARCHAR(100),
+    fk_Id_Usuario INT,
+    fk_id_trending_topic VARCHAR(100),
     Caracteres VARCHAR(255),
     Localizacion VARCHAR(100),
     Foto BLOB,
-    FOREIGN KEY (Id_Usuario)
-        REFERENCES Usuario (Id_Usuario),
-    FOREIGN KEY (Hashtag)
-        REFERENCES TrendingTopic (Hashtag)
+    FOREIGN KEY (fk_Id_Usuario)
+        REFERENCES Usuario (Id_Usuario)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_trending_topic)
+        REFERENCES TrendingTopic (id_trending_topic)
         ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Sigue (
-	Id_Usuario1 INT,
-    Id_Usuario2 INT,
+    fk_Id_Usuario1 INT,
+    fk_Id_Usuario2 INT,
     Mensaje_Privado VARCHAR(255),
-     PRIMARY KEY(Id_Usuario1, Id_Usuario2),
-    FOREIGN KEY (Id_Usuario1)
-        REFERENCES Usuario (Id_Usuario),
-    FOREIGN KEY (Id_Usuario2)
+    PRIMARY KEY (fk_Id_Usuario1 , fk_Id_Usuario2),
+    FOREIGN KEY (fk_Id_Usuario1)
+        REFERENCES Usuario (Id_Usuario)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_Id_Usuario2)
         REFERENCES Usuario (Id_Usuario)
         ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Retweet (
-	Id_Tweet INT,
-    Id_Usuario INT,
-     PRIMARY KEY(Id_Tweet, Id_Usuario),
-    FOREIGN KEY (Id_Tweet)
-        REFERENCES Tweet (Id_Tweet),
-    FOREIGN KEY (Id_Usuario)
+    fk_Id_Tweet INT,
+    fk_Id_Usuario INT,
+    PRIMARY KEY (fk_Id_Tweet , fk_Id_Usuario),
+    FOREIGN KEY (fk_Id_Tweet)
+        REFERENCES Tweet (Id_Tweet)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_Id_Usuario)
         REFERENCES Usuario (Id_Usuario)
         ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -61,23 +64,25 @@ CREATE TABLE IF NOT EXISTS Mensaje (
 );
 
 CREATE TABLE IF NOT EXISTS Envia (
-	Id_Usuario INT,
-    Id_Mensaje INT,
-     PRIMARY KEY(Id_Usuario, Id_Mensaje),
-    FOREIGN KEY (Id_Usuario)
-        REFERENCES Usuario (Id_Usuario),
-    FOREIGN KEY (Id_Mensaje)
+    fk_Id_Usuario INT,
+    fk_Id_Mensaje INT,
+    PRIMARY KEY (fk_Id_Usuario , fk_Id_Mensaje),
+    FOREIGN KEY (fk_Id_Usuario)
+        REFERENCES Usuario (Id_Usuario)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_Id_Mensaje)
         REFERENCES Mensaje (Id_Mensaje)
         ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS Recibe (
-	Id_Usuario INT,
-    Id_Mensaje INT,
-     PRIMARY KEY(Id_Usuario, Id_Mensaje),
-    FOREIGN KEY (Id_Usuario)
-        REFERENCES Usuario (Id_Usuario),
-    FOREIGN KEY (Id_Mensaje)
+    fk_Id_Usuario INT,
+    fk_Id_Mensaje INT,
+    PRIMARY KEY (fk_Id_Usuario , fk_Id_Mensaje),
+    FOREIGN KEY (fk_Id_Usuario)
+        REFERENCES Usuario (Id_Usuario)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (fk_Id_Mensaje)
         REFERENCES Mensaje (Id_Mensaje)
         ON DELETE CASCADE ON UPDATE CASCADE
 );
